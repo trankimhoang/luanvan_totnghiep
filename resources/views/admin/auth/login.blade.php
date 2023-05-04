8b0b91eab0c10b35e3e119ecd26b1287307bf275
trankimhoang <trankimhoang11052000@gmail.com>
2023-05-04 21:05:59 +0700

    fix view reset password admin

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,139 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.master_admin_login')
 
-<head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-    <title>Pages / Login - NiceAdmin Bootstrap Template</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
-
-    <!-- Favicons -->
-    <link href="{{ asset('theme/admin/assets/img/favicon.png') }}" rel="icon">
-    <link href="{{ asset('theme/admin/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link href="{{ asset('theme/admin/assets/font/font.css') }}" rel="stylesheet">
-
-    <!-- Vendor CSS Files -->
-    <link href="{{ asset('theme/admin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('theme/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('theme/admin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('theme/admin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ asset('theme/admin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ asset('theme/admin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('theme/admin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-
-    <!-- Template Main CSS File -->
-    <link href="{{ asset('theme/admin/assets/css/style.css') }}" rel="stylesheet">
-
-    <!-- =======================================================
-    * Template Name: NiceAdmin
-    * Updated: Mar 09 2023 with Bootstrap v5.2.3
-    * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
-    * Author: BootstrapMade.com
-    * License: https://bootstrapmade.com/license/
-    ======================================================== -->
-</head>
-
-<body>
-
-<main>
-    <div class="container">
-
-        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-                        <div class="d-flex justify-content-center py-4">
-                            <a href="index.html" class="logo d-flex align-items-center w-auto">
-                                <img src="{{ asset('theme/admin/assets/img/logo.png') }}" alt="">
-                                <span class="d-none d-lg-block">Admin Manager</span>
-                            </a>
-                        </div><!-- End Logo -->
-
-                        <div class="card mb-3">
-
-                            <div class="card-body">
-
-                                <div class="pt-4 pb-2">
-                                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                                    <p class="text-center small">Enter your email & password to login</p>
-                                </div>
-
-                                @if(session()->has('error'))
-                                    <div class="alert alert-danger">
-                                        {{ session()->get('error') }}
-                                    </div>
-                                @endif
-
-                                <form class="row g-3 needs-validation" novalidate action="{{ route('admin.login.post') }}" method="post">
-                                    @csrf
-                                    <div class="col-12">
-                                        <label for="yourEmail" class="form-label">Email</label>
-                                        <div class="input-group has-validation">
-                                            <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                            <input type="text" name="email" class="form-control" id="yourEmail" required value="{{ old('email') }}">
-                                            @error('email')
-                                            <p class="alert alert-danger">{{ $message }}</p>
-                                            @enderror
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12">
-                                        <label for="yourPassword" class="form-label">Password</label>
-                                        <input type="password" name="password" class="form-control" id="yourPassword" required value="{{ old('password') }}">
-                                        @error('password')
-                                        <p class="alert alert-danger">{{ $message }}</p>
-                                        @enderror
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="remember" value="1" id="rememberMe">
-                                            <label class="form-check-label" for="rememberMe">Remember me</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <a href="{{ route('admin.forget.password.get') }}">Reset Password</a>
-                                    </div>
-                                    <div class="col-12">
-                                        <input type="submit" name="submit" class="btn btn-primary w-100" value="Login">
-
-                                    </div>
-
-                                </form>
-
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
-        </section>
-
+@section('content')
+    <div class="pt-4 pb-2">
+        <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+        <p class="text-center small">Enter your email & password to login</p>
     </div>
-</main><!-- End #main -->
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<!-- Vendor JS Files -->
-<script src="{{ asset('theme/admin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/chart.js/chart.umd.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/echarts/echarts.min.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/quill/quill.min.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
-<script src="{{ asset('theme/admin/assets/vendor/php-email-form/validate.js') }}"></script>
 
-<!-- Template Main JS File -->
-<script src="{{ asset('theme/admin/assets/js/main.js') }}"></script>
+    <form class="row g-3 needs-validation" novalidate action="{{ route('admin.login.post') }}" method="post">
+        @csrf
+        <div class="col-12">
+            <label for="yourEmail" class="form-label">Email</label>
+            <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend">@</span>
+                <input type="text" name="email" class="form-control" id="yourEmail" required value="{{ old('email') }}">
+                @error('email')
+                <p class="alert alert-danger">{{ $message }}</p>
+                @enderror
+            </div>
+        </div>
 
-</body>
+        <div class="col-12">
+            <label for="yourPassword" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="yourPassword" required value="{{ old('password') }}">
+            @error('password')
+            <p class="alert alert-danger">{{ $message }}</p>
+            @enderror
+        </div>
 
-</html>
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" name="remember" value="1" id="rememberMe">
+                <label class="form-check-label" for="rememberMe">Remember me</label>
+            </div>
+        </div>
+        <div class="col-12">
+            <a href="{{ route('admin.forget.password.get') }}">Reset Password</a>
+        </div>
+        <div class="col-12">
+            <input type="submit" name="submit" class="btn btn-primary w-100" value="Login">
+
+        </div>
+
+    </form>
+@endsection
---
+++ resources/views/layouts/master_admin_login.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Pages / Login - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="{{ asset('theme/admin/assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('theme/admin/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="{{ asset('theme/admin/assets/font/font.css') }}" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('theme/admin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/admin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/admin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/admin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/admin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('theme/admin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('theme/admin/assets/css/style.css') }}" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: NiceAdmin
+    * Updated: Mar 09 2023 with Bootstrap v5.2.3
+    * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+<main>
+    <div class="container">
+
+        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                        <div class="d-flex justify-content-center py-4">
+                            <a href="index.html" class="logo d-flex align-items-center w-auto">
+                                <img src="{{ asset('theme/admin/assets/img/logo.png') }}" alt="">
+                                <span class="d-none d-lg-block">Admin Manager</span>
+                            </a>
+                        </div><!-- End Logo -->
+
+                        <div class="card mb-3">
+
+                            <div class="card-body">
+                                <div class="mt-3">
+                                    @if (Session::has('message'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ Session::get('message') }}
+                                        </div>
+                                    @endif
+                                    @if(session()->has('error'))
+                                        <div class="alert alert-danger">
+                                            {{ session()->get('error') }}
+                                        </div>
+                                    @endif
+                                </div>
+                                @yield('content')
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
+</main><!-- End #main -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{ asset('theme/admin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/quill/quill.min.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('theme/admin/assets/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('theme/admin/assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
